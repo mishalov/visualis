43bd88cd0d788bcb06af364e9b673b3561566b68
Evgenii Mishalov <evgenii.mishalov@sap.com>
2023-01-16 15:28:28 +0100

    Update scene configs

--- modules/atomic/get-pixels-coord.ts
+++ modules/atomic/get-pixels-coord.ts
@@ -1,5 +1,5 @@
 import { Dot, SceneConfig } from "../../types";
 
-const getPixelsCoord = (x: number, y: number, sceneConfig: SceneConfig): Dot => ({ x: sceneConfig.stepX * (x - sceneConfig.absoluteMinX), y: sceneConfig.stepY * (sceneConfig.absoluteMaxY - y) })
+const getPixelsCoord = (x: number, y: number, sceneConfig: SceneConfig): Dot => ({ x: Math.round(sceneConfig.stepX * (x - sceneConfig.absoluteMinX)), y: Math.round(sceneConfig.stepY * (sceneConfig.absoluteMaxY - y)) })
 
 export default getPixelsCoord;
